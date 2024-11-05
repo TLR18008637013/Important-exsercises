8103b3862a70e852ffd289f31710365c42d9b5cb
TLR18008637013 <TLR18008637013@outlook.com>
2024-11-05 22:52:33 +0800

    Add files via upload

---
+++ 11.5源.cpp
@@ -0,0 +1,29 @@
+﻿#define ­_CRT_SECURE_NO_WARNINGS 1
+/*#include<stdio.h>
+#include<string.h>
+int MAX(int* p, int len) {
+	int i=0;
+	int value = p[0];
+	for (i = 0; i < len; i++) {
+		if (p[i] > value) {
+			value = p[i];
+		}
+	}
+	return value;
+}
+
+int main() {
+	int MAX(int* p, int len);
+	printf("请输入一个数组：\n");
+	int i=0;
+	int a[6];
+	for (i = 0; i < 6; i++) {
+		scanf("%d", &a[i]);
+	}
+	int len = sizeof(a) / sizeof(int);
+	for (i = 0; i < len; i++) {
+		printf("%4d\n", a[i]);
+	}
+	printf("数组中最大的数是：%d", MAX(a, len));
+	return 0;
+}*/
